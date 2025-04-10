ced333098ab16d8d3a368fd9647429d5597771a8
Ayush <ayush.void0@gmail.com>
2025-04-10 18:14:28 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import { createClient } from '@supabase/supabase-js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_ANON_KEY
+);
